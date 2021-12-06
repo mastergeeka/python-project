d9e15e6a4d3a8b9749198a8fbb7c5632e0e3c909
mastergeeka <93613150+mastergeeka@users.noreply.github.com>
2021-12-05 20:04:48 -0500

    Add files via upload

--- Calculations.py
+++ Calculations.py
@@ -10,15 +10,17 @@ numdist= {}
 districtfile = open("DistrictData.csv","r")
 totvotes=[]
 dlines = districtfile.readlines()
-
+allstates = []
 
 for line in dlines[1:]:
     row = line.strip().split(',')
     name = row[0]
     if name not in statedict:
-        statedict[name]= []  
-    statedict[name].append(row[1])
-    statedict[name].append(row[3]) 
+        statedict[name]= [] 
+        allstates.append(name)
+       
+    statedict[name].append(int(row[1]))
+    statedict[name].append(int(row[3])) 
     totvotes.append(row[4])
      
 
@@ -30,88 +32,109 @@ a = len(totvotes)
 for i in range(a,0,-1):
     if i%2== 1:
         del totvotes[i]
-notposs=[]
-poss=[]
-allclear = []
-repbias = []
-dembias = []
+green = []
+red = []
+blue = []
+white = []
+
 def gerryposs(ST):
+    
     if numdist[ST] < 3:
-        if ST not in notposs:
-            notposs.append(ST)
+        
         return False
-    else:
-        if ST not in poss:
-            poss.append(ST)
+    if numdist[ST] >= 3:
+        
         return True
-for key in numdist:
-    gerryposs(key)
+
 def statewastecount(jkl):
     startslice = 0
-    statenum = 1
+
     for ynot in numdist:
         if ynot !=jkl:
-            startslice += int(numdist[jkl])
-            statenum += 1
+            startslice += int(numdist[ynot])
+            
         else:
             break
     distance = int(numdist[jkl])
+    
     ending= startslice + distance 
     stuff = totvotes[startslice:ending]
-    partyvotes = list(statedict(jkl))
+    
+    partyvotes = list(statedict[jkl])
     demwaste=0
     repwaste=0
     ind = 1
-    for x in range(distance):
-        threshold = int(stuff[0])/2
+    statetotal = 0
+    t =0
+    for g in stuff:
+        statetotal += int(g)
+    
+    for di in range(distance):
+        half = (int(stuff[t]))/2
+        if half % 1 ==0:
+            threshold =half + 1
+        else:
+            threshold =half + 0.5 
+        t+= 1
         demvotes= partyvotes[ind]
         ind+= 2
         repvotes= partyvotes[ind]
         ind +=2
-        if demvotes>repvotes:
+        
+        if demvotes > repvotes:
             wasted_dem= demvotes - threshold
             wasted_rep=repvotes
-        elif demvotes<repvotes:
+        if demvotes<repvotes:
             wasted_dem= demvotes
             wasted_rep=repvotes-threshold
         demwaste += wasted_dem
         repwaste += wasted_rep
     
-    return(demwaste,repwaste)
+    return(demwaste,repwaste,statetotal)
+
+
 def gerrytest(abc):   
-    (x,y)= statewastecount(abc)  
+    (x,y,z)= statewastecount(abc)
+    
     differ = x - y
+    print(abc,x,y,differ)
     if differ < 0:
         differ = differ * (-1)
-        lower = x
-    elif differ > 0:
-        lower = y
-    percent= float(differ/lower)
+     
+        
+    percent= float(differ/z)
+    print(abc,percent)
     if percent >= 0.07:
         return True
     else:
         return False
-def bias(defo):
-    (x,y)= statewastecount(defo)  
-    if gerrytest(defo) == False:
-        print("No Gerrymandering Detected in",defo)
-        if defo not in allclear:
-            allclear.append(defo)
-    
-    elif x > y:
-        print("Detected Gerrymandering Bias Towards Republicans in",defo)
-        repbias.append(defo)
-    elif y < x:
-        print("Detected Gerrymandering Bias Towards Democrats in", defo)
-        dembias.append(defo)
-    
-#for stt in allstatess:
-    #if gerryposs(stt) == True:
-        #bias(stt)
-    #else:
-        #print("Not Enough Districts For Gerrymandering in", stt)
-    
-    
-    
 
+def bias(defo):
+    (x,y,z)= statewastecount(defo) 
+    what =""
+    if gerrytest(defo) == False:
+        what = ("No Gerrymandering Detected in", defo)
+        green.append(defo)
+        
+    
+    else: 
+        if x > y:
+            what = ("Detected Gerrymandering Bias Towards Republicans in", defo)
+            
+        if y > x:
+            what = ("Detected Gerrymandering Bias Towards Democrats in",defo)
+           
+    return(what)
+
+for stt in numdist:
+    if gerryposs(stt) == True:
+        ans = bias(stt)
+    else:
+        ans = ("Not Enough Districts For Gerrymandering in", stt)   
+    
+select = (input("Which state's data do you want to look at?")).upper()
+
+while select not in allstates:
+    print("Not a valid state, Please enter a valid state")
+    #select = (input("Which state's data do you want to look at?")).upper()
 
